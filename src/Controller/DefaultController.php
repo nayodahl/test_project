be4c576adb65870a1ca82005bcbfd15e43441043
nayodahl <anthony.fachaux@gmail.com>
2021-04-28 15:08:26 +0200

    Initial commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(): Response
+    {
+        $response = $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+        
+        return $response;
+    }
+}
